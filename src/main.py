613e440b2a9dafb565b2606611755c7ca379d854
maximilian-ruesch <xammaximus.ruesch@gmail.com>
2023-06-24 21:10:00 +0200

    Convert to hydra application

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ import jax_qgeo
 import jax_qgeo.qcode_enum as qce
 import numpy
 import qgeo
+import hydra
+from omegaconf import DictConfig
 from about_time import about_time
 from jax import jit, value_and_grad
 from jax.experimental.compilation_cache import compilation_cache as cc
@@ -23,14 +25,13 @@ from lib.jax_qgeo import ket, string_permutations_unique
 
 OUTPUT_DIRECTORY = os.path.abspath('../data')
 
-STATE_DIMENSION = 2
-TARGET_DIMENSION = 1
-STEP_SIZE_ALPHA = 0.005
-NUM_ITERATIONS = 30
+@hydra.main(version_base=None, config_path="../conf", config_name=".config.yaml")
+def run(config: DictConfig):
+    states = None
+    match config.states.stateType:
+        case "all": states = AllPureStates(config)
+        case "symmetric": states = SymmetricPureStates(config)
 
-
-def run():
-    states = SymmetricPureStates()
     print(f"[I] Using {states.__class__}")
 
     print("Grading function...")
@@ -53,22 +54,22 @@ def run():
     print(f"Initially grading took {grad_time.duration} seconds")
     print("-----------------------------------------------------------------")
 
-    states.axes = generate_plot(grad_func_jitted)
+    if config.plot:
+        states.axes = generate_plot(grad_func_jitted)
 
-    for _ in alive_progress.alive_it(range(NUM_ITERATIONS), force_tty=True):
+    for _ in alive_progress.alive_it(range(config.iterations), force_tty=True):
         result = grad_func_jitted(state_params)
         print((result[0], numpy.average(result[1]), numpy.linalg.norm(result[1])))
         state_params = states.new_state_params(state_params, result[1])
 
-    print(f"[T] (N k) witness: {math.comb(STATE_DIMENSION, TARGET_DIMENSION)}")
+    print(f"[T] (N k) witness: {math.comb(config.qubitCount, config.target)}")
 
     # Final state normalization
     state_params = states.normalize(state_params)
     final_sector_length = grad_func_jitted(state_params)[0]
     states.final_stats(state_params, final_sector_length=final_sector_length)
 
-    file_name = export_state(states.get_file_prefix(), state_params, final_sector_length)
-    print(f"Saved result to {file_name}")
+    print(f"Saved result to {states.export(state_params, final_sector_length)}")
 
     print("Calculating symmetric properties of the state...")
     print(states.symmetric_bins(state_params))
@@ -113,14 +114,14 @@ def calc_partial_trace(rho, trace_over, n):  # former ptrace_dim
 
     return trace.reshape(d_new, d_new)
 
-def calc_a(n):
+def calc_a(n, t):
     # transform (Rains) unitary to Shor-Laflamme primary enumerator
     x_symbols = symbols("x0:%d" % (n + 1))
     Wp = qce.make_enum(x_symbols, n)
     W = qce.unitary_to_primary(Wp)
     A = qce.W_coeffs(W)
 
-    return A[TARGET_DIMENSION], x_symbols[0:TARGET_DIMENSION + 1]
+    return A[t], x_symbols[0:t + 1]
 
 def all_kRDMs(rho, n, k=2, verbose=False):
     """ generator of all reduced states of size k """
@@ -135,47 +136,46 @@ def all_kRDMs(rho, n, k=2, verbose=False):
 
         yield rho_red
 
-def export_state(file_prefix, state, sector_length):
-    timestamp=int(datetime.timestamp(datetime.now()))
-    file_name = f"{file_prefix}result_s{STATE_DIMENSION}_t{TARGET_DIMENSION}_{timestamp}_{sector_length}.json"
-    file_path = os.path.join(OUTPUT_DIRECTORY, file_name)
-
-    with open(file_path, 'w') as file:
-        json.dump(
-            [str(x) for x in state],
-            file,
-        )
-
-    return file_name
-
 class AllPureStates:
+    _config: DictConfig
+
     axes: Axes = None
+    _file_prefix: str = ""
+
+    def __init__(self, config: DictConfig):
+        self._config = config
 
     def construct_random(self):
-        random_numbers = numpy.random.rand(2 ** STATE_DIMENSION) + 1j * numpy.random.rand(2 ** STATE_DIMENSION)
+        random_numbers = numpy.random.rand(2 ** self._config.qubitCount) + 1j * numpy.random.rand(2 ** self._config.qubitCount)
 
         return self.normalize(random_numbers)
 
     def normalize(self, state_params):
         return state_params / np.linalg.norm(state_params)
 
-    def get_file_prefix(self):
-        return ""
+    def export(self, state, sector_length):
+        timestamp = int(datetime.timestamp(datetime.now()))
+        file_name = f"{self._file_prefix}result_s{self._config.qubitCount}_t{self._config.target}_{timestamp}_{sector_length}.json"
+        file_path = os.path.join(OUTPUT_DIRECTORY, file_name)
+
+        with open(file_path, 'w') as file: json.dump([str(x) for x in state], file)
+
+        return file_name
 
     def stats(self, state_params):
         print(f"Size of the state: {len(state_params)}")
         # print(f"Purity of the state: {qgeo.purity(qgeo.make_dm(state_params))}")
-        print(f"Initial sector length {TARGET_DIMENSION}: {qgeo.sector_len_f(state_params)[TARGET_DIMENSION]}")
+        print(f"Initial sector length {self._config.target}: {qgeo.sector_len_f(state_params)[self._config.target]}")
         print(f"Initial purity: {jax_qgeo.purity(jax_qgeo.make_dm(state_params))}")
 
     def final_stats(self, final_state_params, final_sector_length):
-        exact_sector_length = qgeo.sector_len_f(final_state_params)[TARGET_DIMENSION]
-        print(f"[N] Resulting sector length {TARGET_DIMENSION}: {final_sector_length}")
-        print(f"[N] Original sector length {TARGET_DIMENSION}: {exact_sector_length}")
+        exact_sector_length = qgeo.sector_len_f(final_state_params)[self._config.target]
+        print(f"[N] Resulting sector length {self._config.target}: {final_sector_length}")
+        print(f"[N] Original sector length {self._config.target}: {exact_sector_length}")
         print(f"[N] Resulting purity: {jax_qgeo.purity(jax_qgeo.make_dm(final_state_params))}")
 
     def symmetric_bins(self, state_params):
-        bins = [numpy.array([]) for _ in range(STATE_DIMENSION + 1)]
+        bins = [numpy.array([]) for _ in range(self._config.qubitCount + 1)]
         for index, j in enumerate(state_params):
             bins[index.bit_count()] = numpy.append(bins[index.bit_count()], [j])
 
@@ -187,11 +187,11 @@ class AllPureStates:
         """ obtains sector lenghts / weights trough purities and Rains transform
                     faster, and for arbitrary dimensions
                 """
-        (target_a, x_symbols_for_target) = calc_a(STATE_DIMENSION)
+        (target_a, x_symbols_for_target) = calc_a(self._config.qubitCount, self._config.target)
 
-        Ap = np.zeros(TARGET_DIMENSION + 1)
-        for k in range(TARGET_DIMENSION + 1):
-            for rho_red in all_kRDMs(state_params, n=STATE_DIMENSION, k=k):
+        Ap = np.zeros(self._config.target + 1)
+        for k in range(self._config.target + 1):
+            for rho_red in all_kRDMs(state_params, n=self._config.qubitCount, k=k):
                 Ap = Ap.at[k].set(Ap[k] + purity(rho_red))
 
         return lambdify(x_symbols_for_target, target_a)(*Ap)
@@ -201,18 +201,22 @@ class AllPureStates:
         normal_proj = old_params * np.vdot(gradient, old_params) / np.linalg.norm(old_params)
         tangent_proj = gradient - normal_proj
 
-        self.axes.quiver(*old_params, *(gradient * 0.1), color="g")
-        self.axes.quiver(*old_params, *(tangent_proj * 0.1), color="r")
+        if self._config.plot:
+            self.axes.quiver(*old_params, *(gradient * 0.1), color="g")
+            self.axes.quiver(*old_params, *(tangent_proj * 0.1), color="r")
 
         # step along geodesic on hypersphere
+        step_size = self._config.states.stepSize
         tangent_norm = np.linalg.norm(tangent_proj)
-        new_state = np.cos(tangent_norm * STEP_SIZE_ALPHA) * old_params +\
-                    np.sin(tangent_norm * STEP_SIZE_ALPHA) * (tangent_proj / tangent_norm)
+        new_state = np.cos(tangent_norm * step_size) * old_params +\
+                    np.sin(tangent_norm * step_size) * (tangent_proj / tangent_norm)
 
         return new_state
 
 
 class SymmetricPureStates(AllPureStates):
+    _file_prefix: str = "symm_"
+
     def _denormalized_dicke(self, n, k):
         s = k * '1' + (n - k) * '0'
         s_list = string_permutations_unique(s)
@@ -221,24 +225,21 @@ class SymmetricPureStates(AllPureStates):
 
     def _construct_dicke(self, state_params):
         dicke_states = []
-        for k in range(STATE_DIMENSION + 1):
-            dicke_states.append(self._denormalized_dicke(STATE_DIMENSION, k))
+        for k in range(self._config.qubitCount + 1):
+            dicke_states.append(self._denormalized_dicke(self._config.qubitCount, k))
 
         return np.dot(state_params, np.array(dicke_states))
 
     def construct_random(self):
-        return self.normalize(numpy.random.rand(STATE_DIMENSION + 1))  # + 1j * numpy.random.rand(STATE_DIMENSION + 1)
+        return self.normalize(numpy.random.rand(self._config.qubitCount + 1))  # + 1j * numpy.random.rand(STATE_DIMENSION + 1)
 
     def normalize(self, state_params):
-        blow_up_factor = numpy.sqrt([math.comb(STATE_DIMENSION, k) for k in range(STATE_DIMENSION + 1)])
+        blow_up_factor = numpy.sqrt([math.comb(self._config.qubitCount, k) for k in range(self._config.qubitCount + 1)])
         blown_up_params = state_params * blow_up_factor
 
         normed_blown_up_params = super().normalize(blown_up_params)
 
         return normed_blown_up_params / blow_up_factor
-
-    def get_file_prefix(self):
-        return "symm_"
 
     def stats(self, state_params):
         super().stats(self._construct_dicke(state_params))
@@ -252,20 +253,20 @@ class SymmetricPureStates(AllPureStates):
     def calc_target_sector(self, state_params):
         """ calculates sector lengths only for a single RDM and multiplies by the RDM count """
         rho = self._construct_dicke(state_params)
-        (target_a, x_symbols_for_target) = calc_a(TARGET_DIMENSION)
+        (target_a, x_symbols_for_target) = calc_a(self._config.target, self._config.target)
 
-        trace_over = [i for i in range(TARGET_DIMENSION, STATE_DIMENSION)]
-        rho_red = calc_partial_trace(rho, trace_over=tuple(trace_over), n=STATE_DIMENSION)
+        trace_over = [i for i in range(self._config.target, self._config.qubitCount)]
+        rho_red = calc_partial_trace(rho, trace_over=tuple(trace_over), n=self._config.qubitCount)
 
-        Ap = np.zeros(TARGET_DIMENSION + 1)
-        for k in range(TARGET_DIMENSION + 1):
-            for rho_red_red in all_kRDMs(rho_red, n=TARGET_DIMENSION, k=k):
+        Ap = np.zeros(self._config.target + 1)
+        for k in range(self._config.target + 1):
+            for rho_red_red in all_kRDMs(rho_red, n=self._config.target, k=k):
                 Ap = Ap.at[k].set(Ap[k] + purity(rho_red_red))
 
-        return lambdify(x_symbols_for_target, target_a)(*Ap) * math.comb(STATE_DIMENSION, TARGET_DIMENSION)
+        return lambdify(x_symbols_for_target, target_a)(*Ap) * math.comb(self._config.qubitCount, self._config.target)
 
     def new_state_params(self, old_params, gradient):
-        blow_up_factor = numpy.sqrt([math.comb(STATE_DIMENSION, k) for k in range(STATE_DIMENSION + 1)])
+        blow_up_factor = numpy.sqrt([math.comb(self._config.qubitCount, k) for k in range(self._config.qubitCount + 1)])
 
         blown_up_params = old_params * blow_up_factor
         blown_up_gradient = gradient
